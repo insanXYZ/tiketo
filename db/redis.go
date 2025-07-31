65062dfcf65df5094126246bbf5b48fbde936ee2
insanXYZ <insan.nazal@gmail.com>
2025-07-31 22:28:09 +0700

    add new redis client

--- db/redis.go
+++ db/redis.go
@@ -1,5 +1,26 @@
 package db
 
-func NewRedisClient() {
+import (
+	"context"
+	"os"
+	"strconv"
 
+	"github.com/redis/go-redis/v9"
+)
+
+func NewRedisClient() (*redis.Client, error) {
+	envDb := os.Getenv("REDIS_DB")
+
+	rdDb, err := strconv.Atoi(envDb)
+	if err != nil {
+		return nil, err
+	}
+
+	redisClient := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_ADDRESS"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       rdDb,
+	})
+
+	return redisClient, redisClient.Ping(context.Background()).Err()
 }
